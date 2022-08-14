597e74d72490ff4d5c302c731de654323680beff
rahman351 <itzemerahmansyed@gmail.com>
2022-08-14 18:34:53 +0530

    CSS changes to lef t pane

--- src/LeftPane/LeftPane.js
+++ src/LeftPane/LeftPane.js
@@ -3,10 +3,13 @@ import './LeftPane.css'
 const LeftPane = () => {
     return ( 
         <div className="left">
-            <div className='left-item'>Personal</div>
-            <div className='left-item'>Work</div>
-            <div className='left-item'>Sports</div>
-            <div className='left-item'>Others</div>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Rubik&display=swap" rel="stylesheet"/>
+            <div className='left-item'><div className='bullet'/><span>Personal</span></div>
+            <div className='left-item'><div className='bullet'/><span>Work</span></div>
+            <div className='left-item'><div className='bullet'/><span>Sports</span></div>
+            <div className='left-item'><div className='bullet'/><span>Others</span></div>
         </div>
      );
 }
