3e9f2cd29f49aec44cff9b456f31671fd7026f84
rahman351 <itzemerahmansyed@gmail.com>
2022-08-14 18:43:46 +0530

    Changed note font

--- src/ShowNotes/ShowNotes.js
+++ src/ShowNotes/ShowNotes.js
@@ -3,6 +3,9 @@ import './ShowNotes.css'
 const ShowNotes = (props) => {
     return ( 
         <div className="shownote-parent">
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet"/>
             {props.notes.map(current =>
                 <div className="note-container" style={{backgroundColor: current.color}}>
                     <div className="note-title">{current.title}</div>
