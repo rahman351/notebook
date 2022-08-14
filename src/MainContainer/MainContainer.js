c4fb3b51f245849c2629ca7fd93bcebca9d7bc7d
rahman351 <itzemerahmansyed@gmail.com>
2022-08-14 15:47:26 +0530

    Changed header font

--- src/MainContainer/MainContainer.js
+++ src/MainContainer/MainContainer.js
@@ -4,6 +4,9 @@ import RightPane from '../RightPane/RightPane';
 const MaainContainer = () => {
     return ( 
         <div className="main-container">
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"/>
             <div className="main-top-container">
                 <div className="heading">Simple Note Taking App</div>
                 <div className="info">Source Code : <a href="https://github.com/rahman351/notebook">https://github.com/rahman351/notebook</a></div>
