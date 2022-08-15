28861589097f263c77b9d59f850fae09324893cb
rahman351 <itzemerahmansyed@gmail.com>
2022-08-15 16:46:59 +0530

    Added message for no tags

--- src/ShowNotes/ShowNotes.js
+++ src/ShowNotes/ShowNotes.js
@@ -2,15 +2,17 @@ import './ShowNotes.css'
 
 const ShowNotes = (props) => {
     let displayNotes = props.notes;
+    
     if(props.tag != "All"){
         displayNotes= displayNotes.filter(currentNote=>currentNote.tag == props.tag)
     }
-    
+    let emptyNotes = displayNotes.length==0;
     return ( 
         <div className="shownote-parent">
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
             <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet"/>
             {displayNotes.map(current =>
                 <div className="note-container" style={{backgroundColor: current.color}}>
                     <div className="note-title"><b>{current.title}</b></div>
@@ -18,6 +20,10 @@ const ShowNotes = (props) => {
                     <div className="note-content">Tag : {current.tag}</div>
                 </div>
             )}
+            { 
+                emptyNotes && 
+                    <div className="empty-note-message"> No notes present for the tag: {props.tag}</div>
+            }
         </div>
     );
 }
